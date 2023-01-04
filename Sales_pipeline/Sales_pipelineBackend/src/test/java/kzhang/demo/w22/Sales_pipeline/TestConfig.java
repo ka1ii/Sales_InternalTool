c7c1a0ee4bdf16fe04c80e6e981c08eb090278c6
ka1ii <87829478+ka1ii@users.noreply.github.com>
2023-01-04 16:55:05 -0500

    duplicate

--- Sales_pipeline/Sales_pipelineBackend/src/test/java/kzhang/demo/w22/Sales_pipeline/TestConfig.java
+++
@@ -1,9 +0,0 @@
-package kzhang.demo.w22.Sales_pipeline;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan({ "kzhang.demo.w22.Sales_pipeline" })
-public class TestConfig {
-}
